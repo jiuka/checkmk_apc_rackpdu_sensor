84b869b3bc9d1a98c1a1671d2dd2f5fe8e41c649
Marius Rieder <marius.rieder@durchmesser.ch>
2024-06-14 15:46:06 +0200

    Update from template

--- .devcontainer/symlink.sh
+++ .devcontainer/symlink.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
+PKGNAME=$(python3 -c 'print(eval(open("package").read())["name"])')
+ln -sv $WORKSPACE $OMD_ROOT/local/lib/python3/cmk_addons/plugins/$PKGNAME
 
 for DIR in 'agents' 'checkman' 'checks' 'doc' 'inventory' 'notifications' 'pnp-templates' 'web'; do
     rm -rfv $OMD_ROOT/local/share/check_mk/$DIR
     ln -sv $WORKSPACE/$DIR $OMD_ROOT/local/share/check_mk/$DIR
 done;
 
-rm -rfv $OMD_ROOT/local/lib/check_mk/base/plugins/agent_based
-ln -sv $WORKSPACE/agent_based $OMD_ROOT/local/lib/check_mk/base/plugins/agent_based
+rm -rfv $OMD_ROOT/local/lib/python3/cmk/base/plugins/agent_based
+ln -sv $WORKSPACE/agent_based $OMD_ROOT/local/lib/python3/cmk/base/plugins/agent_based
+
+mkdir -p $OMD_ROOT/local/lib/python3/cmk/base/cee/plugins
+ln -sv $WORKSPACE/bakery $OMD_ROOT/local/lib/python3/cmk/base/cee/plugins/bakery
 
 rm -rfv $OMD_ROOT/local/lib/nagios/plugins
 ln -sv $WORKSPACE/nagios_plugins $OMD_ROOT/local/lib/nagios/plugins
